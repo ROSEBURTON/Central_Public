bd9c64fe8a62f862b144a184c0b447c2767024d2
Rosalinda Burton <114356834+ROSEBURTON@users.noreply.github.com>
2023-12-06 15:15:46 -0700

    Update Central_Public.py

--- Central_Public.py
+++ Central_Public.py
@@ -20,7 +20,7 @@ import os
 
 
 API_URL = "https://api-inference.huggingface.co/models/tiiuae/falcon-7b-instruct"
-API_TOKEN = "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" # Insert your token from Hugging Face in between the " "
+API_TOKEN = "" # Insert your token from Hugging Face in between the " "
 headers = {"Authorization": f"Bearer {API_TOKEN}"}
 # study_initiative = "you"
 central_ai_speaking = False
